906c58822cf9aab6cadfd5d43f8691db9fe6d9b0
Animesh Kumar Singh <74432497+deadpool929@users.noreply.github.com>
2024-03-19 00:24:07 +0530

    Added middlewares and applied checks on mongodbConnection().then().catch().

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import mongoConnection from "./db/index.js";
 import dotenv from "dotenv";
+import app from "./app.js";
 dotenv.config({
   path: "./.env",
 });
-mongoConnection();
+mongoConnection()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error :", err);
+    });
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`App is running at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Error :`, err);
+  });
